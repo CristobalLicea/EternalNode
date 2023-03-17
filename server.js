a4b8d3cbb4b2def7e2cd06ae35d7cd0eebbc7e26
Cristobal Licea <cris.licea@gmail.com>
2023-03-17 15:47:10 -0500

    .

--- server.js
+++ server.js
@@ -202,11 +202,11 @@ io.on('connection', (socket) => {
 
     battleshipState[data.code].players[socket.number].name = data.name;
 
-    io.to(socket.id).emit('battleshipState', JSON.stringify(battleshipState[gameCode].players[socket.number]));
+    //io.to(socket.id).emit('battleshipState', JSON.stringify(battleshipState[gameCode].players[socket.number]));
 
-    setTimeout(() => {
-      io.sockets.in(roomName).emit('battleshipPlace', JSON.stringify(battleshipState[gameCode].units))
-    }, 1000);
+    //setTimeout(() => {
+    //  io.sockets.in(roomName).emit('battleshipPlace', JSON.stringify(battleshipState[gameCode].units))
+    //}, 1000);
 
     /*const emitBattleshipState = (roomName, battleshipState) => {
       io.sockets.in(roomName).emit('battleshipState', JSON.stringify(battleshipState));
